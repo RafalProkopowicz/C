63a6cbc286f027511045b38c3bfbc0c8aab97867
Yuzukiko <32813897+RafalProkopowicz@users.noreply.github.com>
2018-04-14 14:19:55 +0200

    v1.3

--- graf.py
+++ graf.py
@@ -1,12 +1,26 @@
-# macierz sasiedztwa
-tab = [[0, 1, 0, 0, 0],
-       [1, 0, 1, 1, 1],
-       [0, 1, 0, 1, 0],
-       [0, 1, 1, 0, 1],
-       [0, 1, 0, 1, 0]]
+# # macierz sasiedztwa
+# tab = [[0, 1, 0, 0, 0],
+#        [1, 0, 1, 1, 1],
+#        [0, 1, 0, 1, 0],
+#        [0, 1, 1, 0, 1],
+#        [0, 1, 0, 1, 0]]
 
-size = tab.__len__()
+size = int(raw_input("rozmiar tablicy: "))
+tab = [[None] * size for i in range(size)]
+
+st = []
 k = 0
+i = 0
+
+for x in range(0, size):
+    for y in range(x, size):
+        if x == y:
+            tab[x][y] = 0
+        elif tab[x][y] == None:
+            print(tab[x])
+            print "wartosc [", x, "][", y, "]"
+            tab[x][y] = int(input())
+            tab[y][x] = tab[x][y]
 
 # wyswietlenie tablicy
 for x in range(0, size):
@@ -14,9 +28,18 @@ for x in range(0, size):
 
 # przejscie po tablicy
 for x in range(0, size):
-    for y in range(x + 1, size):
+    st.append(i)
+    i = 0
+    for y in range(0, size):
         if tab[x][y] == 1:
             k += 1
+            i += 1
+st.append(i)
 
+# wyswietlenie wartosci
 print "wierzcholki: ",  size
 print "krawedzie: ", k
+for x in range(1, st.__len__()):
+    print "stopien wierzcholka [", x, "] =", st[x]
+st.sort()
+print "stopien grafu: ", st[st.__len__() - 1]
